ba3e0061b8f59ac85df74f2b655b7ade088690c0
gerikov <gerikov.mining@gmail.com>
2023-02-09 16:39:48 +0100

    theme config with mui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function app() {
-  return <div className="app"></div>;
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseLine, ThemeProvider } from "@mui/material";
+
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default app;
+export default App;
